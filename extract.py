8745b4c1b5cff224b9d6e109821812dcbfc0b4ec
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-15 15:25:57 -0800

    cuppy

--- extract.py
+++ extract.py
@@ -4,45 +4,54 @@
 import logging
 import quopri
 import sys
+from email import message_from_file
 from pathlib import Path
 from urllib.parse import urlparse
 
-from email import message_from_file
 from bs4 import BeautifulSoup
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
 def log(msg):
     logging.info(msg)
 
-MAGIC_EXT = 'mhtml.blink'
-DEFAULT_DECODE = 'latin1'
+
+MAGIC_EXT = "mhtml.blink"
+DEFAULT_DECODE = "latin1"
+
 
 def unquote(quoted):
     decoded = quopri.decodestring(quoted)
     content = decoded.decode(DEFAULT_DECODE)
     return content
 
+
 def extract_file_ext(file_name):
-    return Path(file_name).suffix.replace('.','')
+    return Path(file_name).suffix.replace(".", "")
+
 
 def extract_filename(file_path, ctype):
     file_name = file_path.name  # myfile.png
     ext = extract_file_ext(file_name)
-    if ctype[-1] == 'svg+xml': 
-        ctype.append('svg')
+    if ctype[-1] == "svg+xml":
+        ctype.append("svg")
 
-    if ext in ctype: 
-        return file_name 
+    if ext in ctype:
+        return file_name
 
-    split = file_name.split('@')
+    split = file_name.split("@")
     if split[-1] == MAGIC_EXT:
-        return f'{split[-0]}.css'
+        return f"{split[-0]}.css"
 
-    return f'{file_name}.{ctype[-1]}'
+    return f"{file_name}.{ctype[-1]}"
 
-class Extract():
+
+class Extract:
     def __init__(self, source_file):
-        with open(source_file, 'r') as f:
+        with open(source_file, "r") as f:
             self.msg = message_from_file(f)
 
         self.html = None
@@ -56,12 +65,12 @@ class Extract():
         return list(self.attrs.keys())
 
     def replace_filename(self, uri, file_name):
-        local_file = f'./{file_name}'
+        local_file = f"./{file_name}"
         link = self.get(href=uri)
         if link:
-            link['href'] = local_file
+            link["href"] = local_file
             return local_file
-        logging.warning(f'replace_filename.not-found.href={uri}')
+        logging.warning(f"replace_filename.not-found.href={uri}")
 
     def add_file(self, uri, ctype):
         sections = urlparse(uri)
@@ -80,12 +89,14 @@ class Extract():
         self.replace_filename(uri, file_name)
         return attrs
 
-    def save(self, dest='.'):
+    def save(self, dest="."):
         root = Path(dest) / self.folder
         root.mkdir(exist_ok=True)
-        def write(f, s): (root / f).write_text(s)
 
-        write('index.html', str(self))
+        def write(f, s):
+            (root / f).write_text(s)
+
+        write("index.html", str(self))
         for file, data in self.payloads.items():
             write(file, data)
 
@@ -101,13 +112,13 @@ class Extract():
         pass
 
     def parse_part(self, part):
-        ctype = part.get('Content-Type').split('/')
-        quoted = part.get('Content-Transfer-Encoding') == 'quoted-printable'
-        uri = part.get('Content-Location')
+        ctype = part.get("Content-Type").split("/")
+        quoted = part.get("Content-Transfer-Encoding") == "quoted-printable"
+        uri = part.get("Content-Location")
         raw_payload = part.get_payload()
         payload = unquote(raw_payload) if quoted else raw_payload
 
-        if 'html' in ctype:
+        if "html" in ctype:
             assert not self.html
             self.folder = Path(uri).name
             self.raw_html = raw_payload
@@ -119,12 +130,13 @@ class Extract():
             logging.debug(f'file_name {attrs["name"]}')
 
     def __str__(self):
-        return str(self.soup) if self.soup else "Extract<None>" # .prettify()
+        return str(self.soup) if self.soup else "Extract<None>"  # .prettify()
 
     def print_text(self):
         print(self.msg.preamble)
         print(self)
         print(self.msg.epilogue)
+
 
 def main():
     args = sys.argv
@@ -138,5 +150,5 @@ def main():
     log(folder)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
