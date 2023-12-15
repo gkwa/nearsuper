7deb1d68cdcbd01b72ad9719d87abc12c1ef92d0
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-15 15:25:26 -0800

    Original

--- extract.py
+++ extract.py
@@ -1,60 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import email
 import logging
-import pathlib
 import quopri
 import sys
-import time
-import urllib.parse
+from pathlib import Path
+from urllib.parse import urlparse
 
-import bs4
+from email import message_from_file
+from bs4 import BeautifulSoup
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(pathname)s:%(lineno)d - %(asctime)s - %(levelname)s - %(message)s",
-)
-
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 def log(msg):
     logging.info(msg)
 
-
-MAGIC_EXT = "mhtml.blink"
-DEFAULT_DECODE = "latin1"
-
+MAGIC_EXT = 'mhtml.blink'
+DEFAULT_DECODE = 'latin1'
 
 def unquote(quoted):
     decoded = quopri.decodestring(quoted)
     content = decoded.decode(DEFAULT_DECODE)
     return content
 
-
 def extract_file_ext(file_name):
-    return pathlib.Path(file_name).suffix.replace(".", "")
-
+    return Path(file_name).suffix.replace('.','')
 
 def extract_filename(file_path, ctype):
     file_name = file_path.name  # myfile.png
     ext = extract_file_ext(file_name)
-    if ctype[-1] == "svg+xml":
-        ctype.append("svg")
+    if ctype[-1] == 'svg+xml': 
+        ctype.append('svg')
 
-    if ext in ctype:
-        return file_name
+    if ext in ctype: 
+        return file_name 
 
-    split = file_name.split("@")
+    split = file_name.split('@')
     if split[-1] == MAGIC_EXT:
-        return f"{split[-0]}.css"
+        return f'{split[-0]}.css'
 
-    return f"{file_name}.{ctype[-1]}"
+    return f'{file_name}.{ctype[-1]}'
 
-
-class Extract:
+class Extract():
     def __init__(self, source_file):
-        with open(source_file, "r") as f:
-            self.msg = email.message_from_file(f)
+        with open(source_file, 'r') as f:
+            self.msg = message_from_file(f)
 
         self.html = None
         self.attrs = {}
@@ -67,16 +56,16 @@ class Extract:
         return list(self.attrs.keys())
 
     def replace_filename(self, uri, file_name):
-        local_file = f"./{file_name}"
+        local_file = f'./{file_name}'
         link = self.get(href=uri)
         if link:
-            link["href"] = local_file
+            link['href'] = local_file
             return local_file
-        logging.warning(f"replace_filename.not-found.href={uri}")
+        logging.warning(f'replace_filename.not-found.href={uri}')
 
     def add_file(self, uri, ctype):
-        sections = urllib.parse.urlparse(uri)
-        file_path = pathlib.Path(sections.path)
+        sections = urlparse(uri)
+        file_path = Path(sections.path)
         file_name = extract_filename(file_path, ctype)
         file_ext = extract_file_ext(file_name)
 
@@ -91,14 +80,12 @@ class Extract:
         self.replace_filename(uri, file_name)
         return attrs
 
-    def save(self, dest="."):
-        root = pathlib.Path(dest) / self.folder
+    def save(self, dest='.'):
+        root = Path(dest) / self.folder
         root.mkdir(exist_ok=True)
+        def write(f, s): (root / f).write_text(s)
 
-        def write(f, s):
-            (root / f).write_text(s)
-
-        write("index.html", str(self))
+        write('index.html', str(self))
         for file, data in self.payloads.items():
             write(file, data)
 
@@ -114,37 +101,30 @@ class Extract:
         pass
 
     def parse_part(self, part):
-        ctype = part.get("Content-Type").split("/")
-        quoted = part.get("Content-Transfer-Encoding") == "quoted-printable"
-        uri = part.get("Content-Location")
+        ctype = part.get('Content-Type').split('/')
+        quoted = part.get('Content-Transfer-Encoding') == 'quoted-printable'
+        uri = part.get('Content-Location')
         raw_payload = part.get_payload()
         payload = unquote(raw_payload) if quoted else raw_payload
 
-        data_dir = pathlib.Path("data")
-        data_dir.mkdir(exist_ok=True, parents=True)
-
-        if "html" in ctype:
-            # assert not self.html
-            self.folder = pathlib.Path(uri or data_dir).name
+        if 'html' in ctype:
+            assert not self.html
+            self.folder = Path(uri).name
             self.raw_html = raw_payload
             self.html = payload
-            self.soup = bs4.BeautifulSoup(payload, features="html.parser")
-            timestamp = int(time.time() * 1_000_000)
-            file_path = pathlib.Path(f"out-{ctype[0]}-{ctype[1]}-{timestamp}.html")
-            pathlib.Path(file_path).write_text(str(self.soup))
+            self.soup = BeautifulSoup(payload, features="html.parser")
         else:
             attrs = self.add_file(uri, ctype)
             self.payloads[attrs["name"]] = payload
             logging.debug(f'file_name {attrs["name"]}')
 
     def __str__(self):
-        return str(self.soup) if self.soup else "Extract<None>"  # .prettify()
+        return str(self.soup) if self.soup else "Extract<None>" # .prettify()
 
     def print_text(self):
         print(self.msg.preamble)
         print(self)
         print(self.msg.epilogue)
-
 
 def main():
     args = sys.argv
@@ -158,5 +138,5 @@ def main():
     log(folder)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
